ccbdb1cb1903c40c0ce846ee8e874e6972450d6e
Jeffrey Crowley <jeffcrow@byu.edu>
2024-01-27 14:07:39 -0700

    Create frontend project & add basic search bar

---
+++ webapp/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from '~/pages/index.vue'
+
+export default {
+  components: {
+    Home
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ webapp/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input type="text" v-model="searchQuery" placeholder="Enter movie title">
+    <button @click="searchMovies">Search</button>
+
+    <div v-if="searchResults.length > 0">
+      <div v-for="movie in searchResults" :key="movie.id">
+        <h2>{{ movie.title }}</h2>
+        <img :src="movie.poster" alt="Movie Poster">
+        <p>{{ movie.popularity }}</p>
+        <ul>
+          <li v-for="recommendation in movie.recommendations" :key="recommendation.id">
+            {{ recommendation.title }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      searchResults: []
+    };
+  },
+  methods: {
+    searchMovies() {
+      // Call your web service here with the searchQuery
+      // and update the searchResults array with the response
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
