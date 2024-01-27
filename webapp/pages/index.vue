2c324cc03bef1a3295fc5457f9cdf13815fa0f86
Jeffrey Crowley <jeffcrow@byu.edu>
2024-01-27 15:01:13 -0700

    Add Tailwind & some styling

--- webapp/pages/index.vue
+++ webapp/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <input type="text" v-model="searchQuery" placeholder="Enter movie title">
-    <button @click="searchMovies">Search</button>
+  <div class="flex flex-col items-center justify-center min-h-screen gap-10">
+    <input class="shadow appearance-none border rounded py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline w-3/4 max-width 5/6" type="text" v-model="searchQuery" placeholder="Enter movie title">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:shadow-outline" @click="searchMovies">Search</button>
 
     <div v-if="searchResults.length > 0">
       <div v-for="movie in searchResults" :key="movie.id">
@@ -28,8 +28,7 @@ export default {
   },
   methods: {
     searchMovies() {
-      // Call your web service here with the searchQuery
-      // and update the searchResults array with the response
+      // Call web service and update the searchResults array with response
     }
   }
 };
---
+++ webapp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx,vue}',
+    './app.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
